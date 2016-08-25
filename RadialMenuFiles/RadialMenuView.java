38db888a4a5de2d9ba3fb625cefd0672079ad21e
tulipanh <htulipan@gmail.com>
2016-08-24 19:46:36 -0500

    Changed way the View is drawn so that each section is drawn separately, allowing greater customization. Also added highlighting and refactored so that less work is done in the onDraw() function.

--- RadialMenuFiles/RadialMenuView.java
+++ RadialMenuFiles/RadialMenuView.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -32,7 +33,17 @@ public class RadialMenuView extends View {
     private int outerRadius;
     private Paint bodyPaint = new Paint();
     private Paint framePaint = new Paint();
+    private Paint highlightPaint = new Paint();
     private Paint textPaint = new Paint();
+
+    private Point startOrig;
+    private Point endOrig;
+    private Point start;
+    private Point end;
+    private RectF outerCircleRect;
+    private RectF innerCircleRect;
+    private Path[] sectionPaths;
+    private Path[] textPaths;
 
     public RadialMenuView(Context context) {
         super(context);
@@ -67,9 +78,17 @@ public class RadialMenuView extends View {
         // Could have option to set style with a special style in styles.xml, just doing it manually for now
         bodyPaint.setColor(Color.argb(200, 150, 150, 150));
         framePaint.setColor(Color.argb(200, 30, 30, 30));
+        highlightPaint.setColor(Color.argb(180, 240, 240, 0));
         textPaint.setColor(Color.argb(255, 0, 0, 0));
         textPaint.setTypeface(Typeface.MONOSPACE);
-        textPaint.setTextSize(20f);
+        textPaint.setTextSize(40f);
+        bodyPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        framePaint.setStyle(Paint.Style.STROKE);
+        framePaint.setStrokeWidth(6f);
+        framePaint.setStrokeJoin(Paint.Join.ROUND);
+        highlightPaint.setStyle(Paint.Style.STROKE);
+        highlightPaint.setStrokeJoin(Paint.Join.ROUND);
+        highlightPaint.setStrokeWidth(12f);
         mActive = 0;
     }
 
@@ -80,48 +99,72 @@ public class RadialMenuView extends View {
         mOrigin = new Point(getWidth()/2, getHeight()/2);
         innerRadius = getWidth()/6;
         outerRadius = getWidth()/2;
-    }
 
-    @Override
-    public void onDraw(Canvas canvas) {
-        bodyPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        canvas.drawCircle(mOrigin.x, mOrigin.y, outerRadius - 4, bodyPaint);
+        startOrig = new Point(0, innerRadius);
+        endOrig = new Point(0, outerRadius - 4);
+        start = new Point(startOrig);
+        end = new Point(endOrig);
 
-        framePaint.setStyle(Paint.Style.STROKE);
-        framePaint.setStrokeWidth(6f);
-        framePaint.setStrokeJoin(Paint.Join.ROUND);
+        outerCircleRect = new RectF(4, 4, getWidth()-4, getHeight()-4);
+        innerCircleRect = new RectF(getWidth()/3, getHeight()/3, 2*getWidth()/3, 2*getHeight()/3);
 
-        canvas.drawCircle(mOrigin.x, mOrigin.y, outerRadius - 4, framePaint);
-        canvas.drawCircle(mOrigin.x, mOrigin.y, innerRadius, framePaint);
-
-        framePaint.setStrokeWidth(3f);
-        Point startOrig = new Point(0, innerRadius);
-        Point endOrig = new Point(0, outerRadius - 4);
-        Point start = new Point(startOrig);
-        Point end = new Point(endOrig);
-
-        for (int i = 1; i <= mNumOptions; i++) {
-            canvas.drawLine(mOrigin.x+start.x, mOrigin.y+start.y, mOrigin.x+end.x, mOrigin.y+end.y, framePaint);
-            start = rotate(startOrig, i);
-            end = rotate(endOrig, i);
+        sectionPaths = new Path[mNumOptions];
+        for (int i = 0; i < sectionPaths.length; i++) {
+            sectionPaths[i] = new Path();
+            sectionPaths[i].moveTo(mOrigin.x+start.x, mOrigin.y+start.y);
+            sectionPaths[i].lineTo(mOrigin.x+end.x, mOrigin.y+end.y);
+            start = rotate(startOrig, i+1);
+            end = rotate(endOrig, i+1);
+            sectionPaths[i].arcTo(outerCircleRect, 90f-(360f/mNumOptions)*i, -(360f/mNumOptions));
+            sectionPaths[i].lineTo(mOrigin.x+start.x, mOrigin.y+start.y);
+            sectionPaths[i].arcTo(innerCircleRect, 90f-(360f/mNumOptions)*(i+1), 360f/mNumOptions);
         }
 
+        textPaths = new Path[mNumOptions];
         startOrig.set((int) (startOrig.x*Math.cos(Math.PI/mNumOptions) + startOrig.y*Math.sin(Math.PI/mNumOptions))
                 , (int) (-1*startOrig.x*Math.sin(Math.PI/mNumOptions) + startOrig.y*Math.cos(Math.PI/mNumOptions)));
 
         endOrig.set((int) (endOrig.x*Math.cos(Math.PI/mNumOptions) + endOrig.y*Math.sin(Math.PI/mNumOptions))
                 , (int) (-1*endOrig.x*Math.sin(Math.PI/mNumOptions) + endOrig.y*Math.cos(Math.PI/mNumOptions)));
 
-        start = new Point(startOrig);
-        end = new Point(endOrig);
+        start.set(startOrig.x, startOrig.y);
+        end.set(endOrig.x, endOrig.y);
 
         for (int i = 1; i <= mNumOptions; i++) {
-            Path textPath = new Path();
-            textPath.moveTo(mOrigin.x+start.x, mOrigin.y+start.y);
-            textPath.lineTo(mOrigin.x + end.x, mOrigin.y + end.y);
-            canvas.drawTextOnPath(mOptions[i - 1], textPath, 10f, 0f, textPaint);
+            textPaths[i-1] = new Path();
+            textPaths[i-1].moveTo(mOrigin.x+start.x, mOrigin.y+start.y);
+            textPaths[i-1].lineTo(mOrigin.x + end.x, mOrigin.y + end.y);
             start = rotate(startOrig, i);
             end = rotate(endOrig, i);
+        }
+
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.drawCircle(mOrigin.x, mOrigin.y, innerRadius, bodyPaint);
+        canvas.drawCircle(mOrigin.x, mOrigin.y, innerRadius, framePaint);
+
+        for (Path p : sectionPaths) {
+            canvas.drawPath(p, bodyPaint);
+        }
+
+        for (Path p : sectionPaths) {
+            canvas.drawPath(p, framePaint);
+        }
+
+        if (mActive == 0) {
+            canvas.drawCircle(mOrigin.x, mOrigin.y, innerRadius, highlightPaint);
+            canvas.drawCircle(mOrigin.x, mOrigin.y, innerRadius, bodyPaint);
+            canvas.drawCircle(mOrigin.x, mOrigin.y, innerRadius, framePaint);
+        } else {
+            canvas.drawPath(sectionPaths[mActive-1], highlightPaint);
+            canvas.drawPath(sectionPaths[mActive-1], bodyPaint);
+            canvas.drawPath(sectionPaths[mActive-1], framePaint);
+        }
+
+        for (int i = 0; i < mNumOptions; i++) {
+            canvas.drawTextOnPath(mOptions[i], textPaths[i], 10f, 0f, textPaint);
         }
     }
 
@@ -161,12 +204,16 @@ public class RadialMenuView extends View {
                  * and highlight it somehow.
                  */
                 if (distance < innerRadius) {
-                    if(mActive != 0) mActive = 0;
+                    if(mActive != 0) {
+                        mActive = 0;
+                        invalidate();
+                    }
                 } else {
                     double percent = angle/(2*Math.PI);
                     int sectionNumber = (int) (percent / (1.0d/mNumOptions)) + 1;
                     if (mActive != sectionNumber) {
                         mActive = sectionNumber;
+                        invalidate();
                     }
                 }
                 cont = true;
